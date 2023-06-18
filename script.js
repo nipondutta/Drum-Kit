5a77ab10e707d209fbbc471329e93dd973e0ce1e
nipondutta <103346472+nipondutta@users.noreply.github.com>
2023-06-18 22:41:47 +0530

    Added index.html

---
+++ script.js
@@ -0,0 +1,46 @@
+for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+        
+        makesound(this.innerHTML);
+        addanimation(this.innerHTML);
+    });
+}
+document.addEventListener("keypress",function(event){
+    makesound(event.key);
+    addanimation(event.key)
+});
+function makesound(key){
+    switch (key) {
+        case "w":var audio=new Audio("tom-1.mp3");
+            audio.play();
+            break;
+        case "a":var audio=new Audio("tom-2.mp3");
+            audio.play();
+            break;
+        case "s":var audio=new Audio("tom-3.mp3");
+            audio.play();
+            break;
+        case "d":var audio=new Audio("tom-4.mp3");
+            audio.play();
+            break;
+        case "j":var audio=new Audio("snare.mp3");
+            audio.play();
+            break;
+        case "k":var audio=new Audio("crash.mp3");
+            audio.play();
+            break;
+        case "l":var audio=new Audio("kick-bass.mp3");
+            audio.play();
+            break;    
+        default:console.log("Invalid selection!");
+            break;
+    }
+}
+
+function addanimation(key){
+    var active=document.querySelector("."+key);
+    active.classList.add("pressed");
+    setTimeout(function(){
+        active.classList.remove("pressed");
+    }, 200);
+}
